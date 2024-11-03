78071109b9151575c94f05d3410cf2c1b2363c52
Mayank Bansal <mayankbansal@Mayanks-MacBook-Air.local>
2024-11-03 17:45:35 +0530

    Cash Flow Minimize added

--- code.c
+++ code.c
@@ -27,55 +27,21 @@ int userCount = 0;
 Expense expenses[MAX_USERS];
 int expenseCount = 0;
 
-void handleUserInput(){
-    int choice;
-    scanf("%d", &choice);
-    switch (choice) {
-        case 1:
-            addUser();
-            break;
-        case 2:
-            addExpense();
-            break;
-        case 3:
-            printBalances();
-            break;
-        case 4:
-            minimizeCashFlow();
-            break;
-        case 5:
-            printf("Exiting the system. Goodbye!\n");
-            exit(0);
-        default:
-            printf("Invalid choice! Please try again.\n");
-    }
-}
-
-void printMenu(){
-    printf("\nMenu:\n");
-    printf("1. Add User\n");
-    printf("2. Add Expense\n");
-    printf("3. Print Balances\n");
-    printf("4. Minimize Cash Flow\n");
-    printf("5. Exit\n");
-    printf("Enter your choice: ");
-}
-
 void addUser(){
     char name[50];
     printf("Enter the name of the user:- ");
     scanf("%s", name);
-    userCount++;
     users[userCount].id = userCount + 1;
     strcpy(users[userCount].name, name);
     users[userCount].balance = 0;
+    userCount++;
     printf("User %s added successfully with ID %d\n", name, users[userCount - 1].id);
 }
 
-int main(){
-    while(1){
-        printMenu();
-        handleUserInput();
+void printUsers(){
+    printf("List of users:\n");
+    for (int i = 0; i < userCount; i++) {
+        printf("ID: %d, Name: %s\n", users[i].id, users[i].name);
     }
 }
 
@@ -128,9 +94,100 @@ void addExpense(){
     printf("Expense added successfully: %s - Amount: %.2f paid by %s\n", description, amount, users[paidBy - 1].name);
 }
 
-void printUsers(){
-    printf("List of users:\n");
+
+void printBalance(){
+    printf("\nUser Balances:\n");
     for (int i = 0; i < userCount; i++) {
-        printf("Id: %d, Name: %s\n", users[i].id, users[i].name);
+        printf("%s: %.2f\n", users[i].name, users[i].balance);
+    }
+}
+void calculateNetBalances(double netBalance[]) {
+    for (int i = 0; i < userCount; i++) {
+        netBalance[i] = users[i].balance;
+    }
+}
+
+int findMaxCreditor(double netBalance[]) {
+    int maxIdx = 0;
+    for (int i = 1; i < userCount; i++) {
+        if (netBalance[i] > netBalance[maxIdx]) {
+            maxIdx = i;
+        }
+    }
+    return maxIdx;
+}
+
+int findMaxDebtor(double netBalance[]) {
+    int maxIdx = 0;
+    for (int i = 1; i < userCount; i++) {
+        if (netBalance[i] < netBalance[maxIdx]) {
+            maxIdx = i;
+        }
+    }
+    return maxIdx;
+}
+
+void minimizeCashFlow() {
+    double netBalance[MAX_USERS] = {0};
+    double epsilon = 1e-2;
+
+    calculateNetBalances(netBalance);
+
+    while (1) {
+        int maxCreditor = findMaxCreditor(netBalance);
+        int maxDebtor = findMaxDebtor(netBalance);
+
+        if (fabs(netBalance[maxCreditor]) < epsilon && fabs(netBalance[maxDebtor]) < epsilon) {
+            printf("All transactions settled.\n");
+            break;
+        }
+
+        double settlementAmount = (netBalance[maxCreditor] < -netBalance[maxDebtor]) ? netBalance[maxCreditor] : -netBalance[maxDebtor];
+
+        netBalance[maxCreditor] -= settlementAmount;
+        netBalance[maxDebtor] += settlementAmount;
+
+        printf("%s pays %.2f to %s\n", users[maxDebtor].name, settlementAmount, users[maxCreditor].name);
+    }
+}
+
+void printMenu(){
+    printf("\nMenu:\n");
+    printf("1. Add User\n");
+    printf("2. Add Expense\n");
+    printf("3. Print Balances\n");
+    printf("4. Minimize Cash Flow\n");
+    printf("5. Exit\n");
+    printf("Enter your choice: ");
+}
+
+void handleUserInput(){
+    int choice;
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            addUser();
+            break;
+        case 2:
+            addExpense();
+            break;
+        case 3:
+            printBalance();
+            break;
+        case 4:
+            minimizeCashFlow();
+            break;
+        case 5:
+            printf("Exiting the system. Goodbye!\n");
+            exit(0);
+        default:
+            printf("Invalid choice! Please try again.\n");
+    }
+}
+
+int main(){
+    while(1){
+        printMenu();
+        handleUserInput();
     }
 }
